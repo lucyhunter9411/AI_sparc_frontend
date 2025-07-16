bcb21124d81b02ddcdab0bd317acbecc35dcdfb6
lucyhunter9411 <superengineer199885@gmail.com>
2025-07-16 08:02:57 +0800

    Refactoring for sevices

--- src/Hooks/useMenu.ts
+++ src/Hooks/useMenu.ts
@@ -10,21 +10,6 @@ interface ApiResponse<T = any> {
   error: string | null;
 }
 
-export function useLectures() {
-  const BASE_URL = `${process.env.NEXT_PUBLIC_V2_SERVER_URL}/lectures/`;
-  console.log("BASE_URL", BASE_URL);
-  const { getAxiosHandler } = useAxiosHandler();
-
-  const handleGetLectures = async (): Promise<ApiResponse> => {
-    const { data, status, error } = await getAxiosHandler({
-      url: BASE_URL,
-    });
-    return { data, status, error };
-  };
-
-  return { handleGetLectures };
-}
-
 export function useTopics() {
   const BASE_URL = `${process.env.NEXT_PUBLIC_V2_SERVER_URL}/topics/`;
   console.log("BASE_URL", BASE_URL);
